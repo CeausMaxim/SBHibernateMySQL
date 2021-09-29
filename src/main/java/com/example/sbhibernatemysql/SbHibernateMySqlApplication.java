02ecfb33388987e985741595fd2a87560b27caaa
CeausMaxim <chaushm@mail.ru>
2021-09-29 15:37:18 +0300

    Initial commit, Creating all packages and one class for service

---
+++ src/main/java/com/example/sbhibernatemysql/SbHibernateMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbhibernatemysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHibernateMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbHibernateMySqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbhibernatemysql/SbHibernateMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbhibernatemysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbHibernateMySqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
