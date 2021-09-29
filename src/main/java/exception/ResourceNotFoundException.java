eae85c257139240ad6d9ccd92c87f24f695b68a8
CeausMaxim <chaushm@mail.ru>
2021-09-29 19:23:14 +0300

    Add class Country Repository, ResourceNotFoundException

---
+++ src/main/java/exception/ResourceNotFoundException.java
@@ -0,0 +1,19 @@
+package exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue){
+        super(resourceName + " not found with" + fieldName + "=" + fieldValue.toString());
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}
--- src/main/java/model/Country.java
+++ src/main/java/model/Country.java
@@ -1,10 +1,12 @@
 package model;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
 
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "country")
 public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
