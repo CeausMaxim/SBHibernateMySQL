904a34083207879eab055fe513550fc5bd0c82e9
CeausMaxim <chaushm@mail.ru>
2021-09-29 19:37:38 +0300

    Add class CountryServiceImpl, interface CountryService, class CountryController

--- src/main/java/service/CountryService.java
+++ src/main/java/service/CountryService.java
@@ -1,4 +1,17 @@
 package service;
 
-public class CountryService {
+import model.Country;
+
+import java.util.List;
+
+public interface CountryService {
+    Country save(Country country);
+
+    List<Country> getAll();
+
+    Country get(long id);
+
+    Country update(Country country, long id);
+
+    void delete(long id);
 }
---
+++ src/main/java/service/impl/CountryServiceImpl.java
@@ -0,0 +1,48 @@
+package service.impl;
+
+import exception.ResourceNotFoundException;
+import model.Country;
+import org.springframework.stereotype.Service;
+import repository.CountryRepository;
+import service.CountryService;
+
+import java.util.List;
+
+@Service
+public class CountryServiceImpl implements CountryService {
+    CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        super();
+        this.countryRepository = countryRepository;
+    }
+
+    @Override
+    public Country save(Country country) {
+        return countryRepository.save(country);
+    }
+
+    @Override
+    public List<Country> getAll() {
+        return countryRepository.findAll();
+    }
+
+    @Override
+    public Country get(long id) {
+        return countryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Country", "id", id));
+    }
+
+    @Override
+    public Country update(Country country, long id) {
+        Country existingCountry = countryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Country", "id", id));
+        existingCountry.setName(country.getName());
+        countryRepository.save(existingCountry);
+        return null;
+    }
+
+    @Override
+    public void delete(long id) {
+        countryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Country", "id", id));
+        countryRepository.deleteById(id);
+    }
+}
